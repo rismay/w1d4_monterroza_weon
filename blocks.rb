4ce14d3fcea0da7a61f1954db2eec2d37ea07fa4
cristian_monterroza <cmonterroza@wrkstrm.me>
2014-04-03 10:41:47 -0700

    Completed Block activities.

--- blocks.rb
+++ blocks.rb
@@ -31,17 +31,46 @@ class Array
     array
   end
 
-  def my_inject (&blk)
-    variable = nil
-    index = 0
+  def my_inject (start = 0, &blk)
+    index = 1
+    variable = self[0]
     while index < self.count
-      array << self[index] if blk.call(self[index])
+      variable = blk.call(variable, self[index])
       index += 1
     end
+    variable
+  end
 
-    array
+  def my_sort! (&blk)
+    sorted = false
+    until sorted
+      sorted = true
+
+      self.each_index do |index|
+        next if (index + 1) == self.count
+        if blk.call(self[index], self[index+1]) > 0
+          self[index], self[index+1] = self[index+1], self[index]
+          sorted = false
+        end
+        index += 1
+      end
+
+    end
+    self
+  end
+
+end
+
+def eval_block(*args, &blk)
+  if blk
+    blk.call(*args)
+  else
+    puts "NO BLOCK GIVEN"
   end
 end
+
+
+
 
 # Random tests
 ["one", "two", "three"].my_each { |string| puts string }
@@ -51,4 +80,13 @@ p ["one", "two", "three"].my_map { |string| string * 2 }
 p ["one", "two", "three"].my_select { |string| string.length <= 3}
 
 # Inject test
-p ["one", "two", "three"].my_inject { |string| string.length <= 3}
+p [1, 2, 3].my_inject { |sum, value| sum + value }  # 6
+
+# Sort! test
+p [3, 2, 1].my_sort! { |num1, num2| num1 <=> num2 } #sort ascending
+p [1, 2, 3].my_sort! { |num1, num2| num2 <=> num1 } #sort descending
+
+# Eval_block test
+p eval_block(1, 2, 3) {|num1, num2, num3| puts "#{num1} #{num2} #{num3}"}
+p eval_block(1, 2, 3)
+
